38ee8ad95ad8394cf28afce6097b5507cd158a7b
Giovanni Gobbi <giovannigobbibusiness@gmail.com>
2020-04-22 11:17:22 +0200

    Add tests for google sign in verification

--- utb/tests/test_add_user.py
+++ utb/tests/test_add_user.py
@@ -1,20 +1,32 @@
-from django.http import HttpResponseBadRequest, HttpResponse, HttpRequest
+from django.http import HttpResponseBadRequest, HttpResponse, HttpRequest, HttpResponseForbidden
 from django.test import TestCase
-from .views import add_user
+from unittest.mock import Mock
+
+from utb import utils
+from utb.views import add_user
+from utb.models import User
 
 
-class ViewsTestCase(TestCase):
+class AddUserTestCreation(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cls_atomics = cls._enter_atomics()
+        # mocks the google_token check
+        utils.check_google_token = Mock(return_value=(True, None))
+
     def test_null_or_empty_uid(self):
         uid = None
         name = "name"
         surname = "surname"
         email = "email@email.com"
         expected = HttpResponseBadRequest("Invalid arguments")
+
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
+
         uid = ""
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
 
     def test_null_or_empty_name(self):
         uid = "uid"
@@ -23,10 +35,11 @@ class ViewsTestCase(TestCase):
         email = "email@email.com"
         expected = HttpResponseBadRequest("Invalid arguments")
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
+
         name = ""
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
 
     def test_null_or_empty_surname(self):
         uid = "uid"
@@ -35,10 +48,11 @@ class ViewsTestCase(TestCase):
         email = "email@email.com"
         expected = HttpResponseBadRequest("Invalid arguments")
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
+
         surname = ""
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
 
     def test_null_or_empty_email(self):
         uid = "uid"
@@ -47,10 +61,11 @@ class ViewsTestCase(TestCase):
         email = None
         expected = HttpResponseBadRequest("Invalid arguments")
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
+
         email = ""
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
 
     def test_invalid_email(self):
         uid = "uid"
@@ -59,9 +74,27 @@ class ViewsTestCase(TestCase):
         email = "email"
         expected = HttpResponseBadRequest("Invalid arguments")
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
+
         email = "valid@email.com"
         expected = HttpResponse(status=200)
         response = add_user(HttpRequest(), uid, name, surname, email)
-        self.assertEqual(response.content, expected.content)
+        self.assertEqual(str(response), str(expected))
 
+    def test_valid_user(self):
+        uid = "uid"
+        name = "name"
+        surname = "surname"
+        email = "valid@email.com"
+        expected = HttpResponse(status=200)
+        response = add_user(HttpRequest(), uid, name, surname, email)
+        self.assertEqual(str(response), str(expected))
+        self.assertTrue(User(uid, name, surname, email) in User.objects.all())
+
+
+class AddUserTestGoogleSignInToken(TestCase):
+    def test_invalid_token(self):
+        utils.check_google_token = Mock(return_value=(False, "Error message"))
+        expected = HttpResponseForbidden("Error message")
+        response = add_user(HttpRequest(), "uid", "name", "surname", "valid@email.com")
+        self.assertEqual(str(response), str(expected))
