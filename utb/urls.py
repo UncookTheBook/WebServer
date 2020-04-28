260690d57a47927a5a9b1e07fe1ffd2ffcc6bd0f
Giovanni Gobbi <giovannigobbibusiness@gmail.com>
2020-04-28 13:52:44 +0200

    Temp for debugging

--- utb/urls.py
+++ utb/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("add_user", views.add_user, name="add_user"),
-    path("get_article", views.get_article, name="get_article")
+    path("get_article", views.get_article, name="get_article"),
+    path("submit_report", views.submit_report, name="submit_report")
 ]
--- utb/views.py
+++ utb/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 
-from .models import User, Article, Website
+from .models import User, Article, Website, Report
 from . import utils
 
 
@@ -71,6 +71,39 @@ def get_article(request):
         status=200
     )
 
+
+def submit_report(request):
+    is_token_valid, error_msg = utils.check_google_token(request)
+    if not is_token_valid:
+        return HttpResponse(error_msg, status=403)
+
+    is_object_present, object_json = utils.get_object(request)
+    if not is_object_present:
+        return HttpResponse(object_json["error"], status=404)
+
+    user_id, article_url, report = object_json["uid"], object_json["url"], object_json["report"]
+    qs = User.objects.filter(id=user_id)
+    if len(qs) == 0:
+        return HttpResponse("User not found", status=404)
+    user = qs[0]
+
+    qs = Article.objects.filter(id=utils.hash_digest(article_url))
+    if len(qs) == 0:
+        return HttpResponse("Article not found", status=404)
+    article = qs[0]
+
+    report = Report(user=user,
+                    article=article,
+                    report_value=report)
+    report.save()
+    if report == "L":
+        article.positive_reports += 1 * user.multiplier
+    else:
+        article.positive_reports += 1 * user.multiplier
+    user.n_reports += 1
+    article.save()
+    user.save()
+    
 
 def add_article(article_id, url, website_name):
     """
