b50740be5c24146f3fb19f4dcd4f2f42be32f18c
Giovanni Gobbi <giovannigobbibusiness@gmail.com>
2020-05-08 10:17:10 +0200

    Security update. Add get_article report

--- utb/views/add_friend.py
+++ utb/views/add_friend.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 
 from utb import utils
 from utb.models import User, Friendship
-from utb.utils import user_exists
 
 
 def handler(request):
@@ -11,25 +10,28 @@ def handler(request):
     :param request: the request
     :return:    HttpResponse 403 if the verification token is invalid
                 HttpResponse 404 if the object is invalid
+                HttpResponse 406 if the user and the friend are the same
                 HttpResponse 201 if the friendship was created
     """
-    is_token_valid, error_msg = utils.check_google_token(request)
+    is_token_valid, message = utils.check_google_token(request)
     if not is_token_valid:
-        return HttpResponse(error_msg, status=403)
+        return HttpResponse(message, status=403)
+    user_id = message
+
+    user = utils.get_user_by_id(user_id)
+    if not user:
+        return HttpResponse("Missing user", status=404)
 
     is_object_present, object_json = utils.get_object(request)
     if not is_object_present:
         return HttpResponse(object_json["error"], status=404)
 
-    user_id, friend_email = object_json["uid"], object_json["friend_email"]
-    if not user_exists(user_id):
-        return HttpResponse("Invalid user", status=404)
+    friend_email = object_json["friend_email"]
 
     qs = User.objects.filter(email=friend_email)
     if len(qs) == 0:
         return HttpResponse("Friend not found", status=404)
 
-    user = User.objects.get(id=user_id)
     friend = qs[0]
     if user == friend:
         return HttpResponse("User and friend must not be the same user", status=406)
--- utb/views/get_leaderboard.py
+++ utb/views/get_leaderboard.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse, JsonResponse
 
 from utb import utils
 from utb.models import User, Friendship
-from utb.utils import user_exists
 
 
 def handler(request):
@@ -13,35 +12,37 @@ def handler(request):
                 HttpResponse 404 if the object is invalid
                 JsonResponse 200 containing the leaderboard if the request went through
     """
-    is_token_valid, error_msg = utils.check_google_token(request)
+    is_token_valid, message = utils.check_google_token(request)
     if not is_token_valid:
-        return HttpResponse(error_msg, status=403)
+        return HttpResponse(message, status=403)
+    user_id = message
+
+    requester = utils.get_user_by_id(user_id)
+    if not requester:
+        return HttpResponse("Missing user", status=404)
 
     is_object_present, object_json = utils.get_object(request)
     if not is_object_present:
         return HttpResponse(object_json["error"], status=404)
 
-    leaderboard_type, uid = object_json["type"], object_json["uid"]
-    if not user_exists(uid):
-        return HttpResponse("Invalid user", status=404)
+    leaderboard_type = object_json["type"]
 
     if leaderboard_type == "GLOBAL":
         user_position = None
         leaderboard = []
         for i, user in enumerate(sorted(list(User.objects.all()), key=lambda u: u.score(), reverse=True), 1):
-            if user.id == uid:
+            if user.id == user_id:
                 user_position = i
             leaderboard.append({"name": user.name, "score": int(user.score())})
         data = {"user_position": user_position, "leaderboard": leaderboard}
         return JsonResponse(data, status=200)
     elif leaderboard_type == "FRIENDS":
-        requester = User.objects.get(id=uid)
         user_position = None
         leaderboard = []
         for i, user in enumerate(sorted(
                 list(map(lambda friendship: friendship.friend, Friendship.objects.filter(user=requester))) + [
-                    requester], key=lambda user: user.score(), reverse=True), 1):
-            if user.id == uid:
+                    requester], key=lambda u: u.score(), reverse=True), 1):
+            if user.id == user_id:
                 user_position = i
             leaderboard.append({"name": user.name, "score": int(user.score())})
         data = {"user_position": user_position, "leaderboard": leaderboard}
